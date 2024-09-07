581a4eb9d5122aa130ac9f7cffc3296467a09393
Moath Mushtaha <moathmushtaha@gmail.com>
2024-09-07 19:10:52 +0300

    Update GetMessagesHistory to check update of incoming message after call

--- tests/Feature/chat/GetMessagesHistoryTest.php
+++ tests/Feature/chat/GetMessagesHistoryTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Chat;
 use App\Models\User;
 
 //guest can't get messages history
@@ -17,4 +18,24 @@ test('authenticated user can get his messages history', function () {
     $response = $this->getJson('/api/chat/messages-history?with_user_id=1');
 
     $response->assertStatus(200);
+});
+
+//After get messages history, all incoming messages should be marked as read
+test('after get messages history, all incoming messages should be marked as read', function () {
+    $from_user_id = User::factory()->create();
+    $to_user_id = User::factory()->create();
+
+    $this->actingAs($to_user_id);
+
+    Chat::factory()->create([
+        'from_user_id' => $from_user_id,
+        'to_user_id' => $to_user_id,
+        'status' => 1
+    ]);
+
+    $response = $this->getJson('/api/chat/messages-history?with_user_id='.$from_user_id->id);
+    $response->assertJsonFragment(['status_count' => 1]);
+
+    $response = $this->getJson('/api/chat/messages-history?with_user_id='.$from_user_id->id);
+    $response->assertJsonFragment(['status_count' => 0]);
 });
