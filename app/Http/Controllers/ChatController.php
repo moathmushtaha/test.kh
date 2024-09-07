47f9c56907688330035bbaf2a6ead2a8697ddadb
Moath Mushtaha <moathmushtaha@gmail.com>
2024-09-07 18:38:31 +0300

    Finish add new message

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,7 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\NewMessageRequest;
+use App\Models\Chat;
+
 class ChatController extends Controller
 {
+    /**
+     * Create a new message
+     *
+     * @param NewMessageRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function newMessage(NewMessageRequest $request)
+    {
+        $chat = Chat::create($request->all());
 
+        return response()->json($chat, 201);
+    }
 }
---
+++ app/Http/Requests/NewMessageRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class NewMessageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'message' => 'required|string|max:255',
+            'to_user_id' => 'required|integer|exists:users,id',
+        ];
+    }
+
+    public function passedValidation()
+    {
+        $this->merge([
+            'from_user_id' => auth()->id()
+        ]);
+    }
+}
