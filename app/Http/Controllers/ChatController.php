851ae1c8dcbcf679d0ffa5823bb66d05fe2f2acb
Moath Mushtaha <moathmushtaha@gmail.com>
2024-09-13 12:38:32 +0300

    Refactor

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -5,73 +5,43 @@ namespace App\Http\Controllers;
 use App\Http\Requests\GetMessagesHistoryRequest;
 use App\Http\Requests\NewMessageRequest;
 use App\Models\Chat;
-use App\Models\User;
-use App\Services\LocationService;
+use App\Services\GeoIPService;
 
 class ChatController extends Controller
 {
-    /**
-     * Create a new message
-     *
-     * @param NewMessageRequest $request
-     * @return \Illuminate\Http\JsonResponse
-     */
+    protected $geoIPService;
+
+    public function __construct(GeoIPService $geoIPService)
+    {
+        $this->geoIPService = $geoIPService;
+    }
+
     public function newMessage(NewMessageRequest $request)
     {
         $chat = Chat::create($request->all());
+        $current_user_location = $this->getCurrentUserLocation($request);
 
-        $current_user_location = (new LocationService())->getLocation($request->latitude, $request->longitude, $request->ip());
         auth()->user()->updateLocation($current_user_location);
 
         return response()->json($chat, 201);
     }
 
-    /**
-     * Get all messages history between the authenticated user and other users
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function MessagesHistory(GetMessagesHistoryRequest $request)
+    public function messagesHistory(GetMessagesHistoryRequest $request)
     {
-        $userId = auth()->id();
+        $user = auth()->user();
         $withUserId = $request->with_user_id;
-        $locationService = new LocationService();
 
-        //Get location for the current user and the user to chat with
-        $current_user_location = $locationService->getLocation($request->latitude, $request->longitude, $request->ip());
-        $with_user_location = User::find($withUserId)->getLocation();
-        $distance = $locationService->calculateDistance($current_user_location, $with_user_location);
+        $messages = $user->getMessagesHistory($withUserId);
+        $with_user = $messages->where('from_user_id', $withUserId)->first()->user;
+        $status_count = $this->getUnreadMessagesCount($messages);
+        $ids_need_update = $this->getIdsNeedUpdate($messages, $withUserId);
+        $messages = $this->getMessageDetails($messages);
 
-        //Update auth user location
-        auth()->user()->updateLocation($current_user_location);
+        $current_user_location = $this->getcurrentUserLocation($request);
+        $distance = $user->getDistanceTo($with_user);
+        $user->updateLocation($current_user_location);
 
-        // Fetch messages between the authenticated user and the specified user
-        $messages = Chat::where(function ($query) use ($withUserId, $userId, $request) {
-            $query->where('from_user_id', $userId)
-                ->where('to_user_id', $withUserId);
-        })->orWhere(function ($query) use ($withUserId, $userId, $request) {
-            $query->where('from_user_id', $withUserId)
-                ->where('to_user_id', $userId);
-        })->orderBy('created_at', 'asc')
-            ->get();
-
-        //Count unread messages for the current user
-        $status_count = $messages->where('to_user_id', auth()->id())->where('status', 1)->count();
-
-        //Get messages list
-        $messages = $messages->map(function ($message) use ($userId) {
-            return [
-                'direction' => $message->from_user_id == $userId ? 'outgoing' : 'incoming',
-                'text' => $message->message,
-                'status' => $message->status,
-            ];
-        });
-
-        //Update the status of incoming messages to read
-        Chat::where('to_user_id', auth()->id())
-            ->where('from_user_id', $withUserId)
-            ->where('status', 1)
-            ->update(['status' => 0]);
+        Chat::whereIn('id', $ids_need_update)->update(['status' => 0]);
 
         return response()->json([
             'messages' => $messages,
@@ -79,5 +49,45 @@ class ChatController extends Controller
             'distance' => $distance
         ]);
     }
+
+    public function getCurrentUserLocation($request)
+    {
+        if (is_null($request->latitude) && is_null($request->longitude)) {
+            return $this->geoIPService->getLocationFromIp($request->ip());
+        }
+
+        return [
+            'latitude' => $request->latitude,
+            'longitude' => $request->longitude,
+        ];
+    }
+
+    public function getUnreadMessagesCount($messages)
+    {
+        return $messages->where('to_user_id', auth()->id())->where('status', 1)->count();
+    }
+
+    public function getIdsNeedUpdate($messages, $withUserId)
+    {
+        return $messages
+            ->where('to_user_id', auth()->id())
+            ->where('from_user_id', $withUserId)
+            ->where('status', 1)
+            ->pluck('id')
+            ->toArray();
+    }
+
+    public function getMessageDetails($messages)
+    {
+        return $messages->map(function ($message) {
+            return [
+                'direction' => $message->from_user_id == auth()->id() ? 'outgoing' : 'incoming',
+                'text' => $message->message,
+                'status' => $message->status,
+            ];
+        });
+    }
+
+
 
 }
